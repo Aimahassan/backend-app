f65b104a23e07924c07ade41bf28e1bfc5a90897
Aimahassan <aimahassan1212@gmaail.com>
2025-09-01 06:12:12 -0700

    Add env file

--- index.mjs
+++ index.mjs
@@ -7,9 +7,15 @@ const app = express();
 
 db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
 
-app.listen(3000,()=>{
-     console.log("Server is ready to use 3000");
+// app.listen(3000,()=>{
+//      console.log("Server is ready to use 3000");
+// })
+app.get((req,res)=>{
+     res.send('server is ready to use')
 })
+
 app.use(cors())
 app.use(express.json())
-app.use('/',router);
+app.use('/',router)
+
+export default app
