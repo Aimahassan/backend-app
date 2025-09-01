cfb71b885624b321c3e247a10a253094d656482b
Aimahassan <aimahassan1212@gmaail.com>
2025-09-01 06:30:07 -0700

    add env file

--- index.mjs
+++ index.mjs
@@ -5,24 +5,24 @@ import cors from "cors";
 
 const app = express();
 
-// Database connection
+// ✅ Connect to DB
 db.connection
   .once("open", () => console.log("✅ Connected to DB"))
   .on("error", (err) => console.error("❌ Error connecting DB -->", err));
 
-// Middlewares
+// ✅ Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// ✅ Test Route
 app.get("/", (req, res) => {
   res.send("Server is ready to use 🚀");
 });
 
-// Routes
+// ✅ Routes
 app.use("/", router);
 
-// Start server
+// ✅ Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
